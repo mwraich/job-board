69c6f2d275b1cf8571d526a2093e6d4351c137f1
Meep <mwraich28@gmail.com>
2022-06-07 11:47:56 -0400

    Create jobs tables and clean up

---
+++ pages/utils.js
@@ -0,0 +1,9 @@
+export default function Utils() {
+    return (
+        <div className="mt-10 ml-20">
+            <h2 className="mb-10 text-xl">
+                Utils
+            </h2>
+        </div>
+    )
+}
